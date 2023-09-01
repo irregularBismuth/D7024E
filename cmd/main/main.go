1b4bd33c9a0021d8bdb541b787a7f887fd5414ac
viktor.smedberg <viktor.smedberg@mobilaris.se>
2023-09-01 12:51:15 +0200

    fixing new path to main.go

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
+    "github.com/irregularBismuth/D7024E/src"
 )
 
 func main(){
     // TODO...
-     
-    fmt.Println("This is random Node: ", rand.Int())
+    var id = src.NewRandomKademliaID() 
+    fmt.Println("This is random Node: ", id)
 }
