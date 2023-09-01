691401c30ae4fe83ff0bd3d131ce3853f4e85cab
viktor.smedberg <viktor.smedberg@mobilaris.se>
2023-09-01 12:33:55 +0200

    changing project structure and dependencies

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -3,11 +3,10 @@ package main
 import (
 	"fmt"
 	"math/rand"
-    "github.com/irregularBismuth/D7024E/src"
 )
 
 func main(){
     // TODO...
-    
+     
     fmt.Println("This is random Node: ", rand.Int())
 }
