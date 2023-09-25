2afa5955970e97019a62fe4e63c1c4c5cae9cff9
irregularBismuth <elliot@live.se>
2023-09-25 08:17:27 +0000

    fix up cli

--- src/cli.go
+++ src/cli.go
@@ -12,7 +12,7 @@ func FindCommands(cmd string){
     r:= regexp.MustCompile("[^\\s]+")
     w:=r.FindAllString(cmd,-1)
     if(w[0]=="Help" || w[0]=="help") {
-        fmt.Println("List of available commands is : ")
+        fmt.Println("List of available commands is : \n\n Put takes one argument object as UTF-8 format you want to store on the network e.g \n Put str123")
     } else if(w[0]=="Put"){
         if(len(w)==2){
             //check 
@@ -20,6 +20,8 @@ func FindCommands(cmd string){
         }else{ 
             fmt.Println("The put command takes in only 1 Argument of the object you want to store on the kademlia network\n")
         }
+    } else if (w[0]=="Exit"){
+        fmt.Println("Terminate node")
     }else{
         fmt.Println("Command not found")
     }
