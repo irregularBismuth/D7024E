ca5aee09868fc17e6e8159315c7a9460efc37f36
irregularBismuth <elliot@live.se>
2023-09-25 17:41:43 +0000

    push changes

--- src/cli.go
+++ src/cli.go
@@ -5,22 +5,26 @@ import(
 "fmt"
 "bufio"
 "regexp"
+"strings"
 )
 
 func FindCommands(cmd string){
     //https://stackoverflow.com/questions/13737745/split-a-string-on-whitespace-in-go
     r:= regexp.MustCompile("[^\\s]+")
     w:=r.FindAllString(cmd,-1)
-    if(w[0]=="Help" || w[0]=="help") {
+    for i,s := range w {
+        w[i] = strings.ToLower(s)
+    }
+    if(w[0]=="help") {
         fmt.Println("List of available commands is : \n\n Put takes one argument object as UTF-8 format you want to store on the network e.g \n Put str123")
-    } else if(w[0]=="Put"){
+    } else if(w[0]=="put"){
         if(len(w)==2){
             //check 
             fmt.Println("Run put cmd")
         }else{ 
             fmt.Println("The put command takes in only 1 Argument of the object you want to store on the kademlia network\n")
         }
-    } else if (w[0]=="Exit"){
+    } else if (w[0]=="exit"){
         fmt.Println("Terminate node")
     }else{
         fmt.Println("Command not found")
@@ -28,17 +32,11 @@ func FindCommands(cmd string){
 
 }
 
-
 func RunCLI() {
-    
-    scanner := bufio.NewScanner(os.Stdin)
-    for scanner.Scan() {
-        line := scanner.Text()
-        //fmt.Println(line)
-        fmt.Println("Enter CLI: via Docker attach Node")
-        //input,_ := reader.ReadString('\n')
-        if(len(line) > 0){ FindCommands(line[:len(line)-1]) }
+    reader := bufio.NewReader(os.Stdin)
+    fmt.Println("Enter CLI: via Docker attach Node")
+    for{
+        input,_ := reader.ReadString('\n')
+        if(len(input) > 0){ FindCommands(input[:len(input)-1]) }
     }
 }
-
-
