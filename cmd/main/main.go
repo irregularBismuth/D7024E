9f05803b62652a2dd3ab15dff59dc90b3fcedd8e
wiiggee1 <viktor@smedberg.eu>
2023-09-25 10:28:47 +0200

    fixing merge conflicts

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -15,28 +15,13 @@ func main(){
     
     //kademliaNode:=src.InitNode(src.GetOutboundIP())
     kademliaNetwork := src.InitNodeNetwork()
-<<<<<<< HEAD
     go kademliaNetwork.ListenServer()
     go kademliaNetwork.HandleResponseChannel() 
     //kademliaNetwork.BootstrapConnect()
     kademliaNetwork.JoinNetwork()
 
- /*   var id = src.NewRandomKademliaID() 
-    a:=src.NewContact(id,GetOutboundIP().String());
-    
-    fmt.Println("This is random Node: ", id)
-        
-   // fmt.Println("BN : ",os.Getenv("BN"))
-    fmt.Println(a.Address);
-    src.Listen("localhost",8888)
-    fmt.Println("132 3212")*/
-
-
-=======
-    go kademliaNetwork.InvokeServer()
     src.RunCLI()
     //kademliaNetwork.BootstrapConnect()
    // go kademliaNetwork.ListenJoin()
->>>>>>> bf87628ae14c1b1c921e773143154eb6233a96c6
     for {}
 }
